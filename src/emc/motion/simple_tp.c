009c04b5601c19d0f658473036e3bfcba471078b
Dewey Garrett <dgarrett@panix.com>
2017-12-05 05:49:00 -0700

    simple_tp: refactor to updated limit3 method
    1) Rewrite of simple_tp.c preserving structure of John Morris
       rewrite of the limit3.comp adapting hal pin names to
       struct simple_tp_t items.
    
    2) Add struct simple_tp_t item for 'disallow_backoff' which
       is useful for special cases (external offsets pid usage)
    
    Tests:
     a) runtests suite
    
     b) axis.ini and other sims for:
        keyboard (nml) jogging (joint and teleop modes)
        halui    (nml) jogging (joint and teleop modes)
        wheel          jogging
    
     c) feature branch dgarr/external_offsets

--- src/emc/motion/simple_tp.c
+++ src/emc/motion/simple_tp.c
@@ -1,75 +1,146 @@
-/********************************************************************
-* Description: simple_tp.c
-*   A simple single axis trajectory planner.  See simple_tp.h for API.
-*
-* Author: jmkasunich
-* License: GPL Version 2
-* Created on:
-* System: Linux
-*
-* Copyright (c) 2004 All rights reserved.
-********************************************************************/
+/* based on limit3.comp by John Morris (zultron)
 
+      limit3 halpin --> struct_tp_t
+      --------          -----------
+      in            --> tp->pos_cmd
+      out           --> tp->curr_pos
+      in_limit      --> notused, removed
+      load          --> removed, use tp->enable for control
+      min_          --> tp->min_pos
+      min_          --> tp->min_pos
+      max_          --> tp->max_pos
+      maxv          --> tp->max_vel
+      maxa          --> tp->max_acc
+
+      limit3 var    --> struct_tp_t
+      -------------     -----------
+      in_pos_old    --> tp->in_pos_old  (new struct item)
+      out_vel_old   --> tp->out_vel_old (new struct item)
+
+      limit3 local  --> struct_tp_t
+      ----------        -----------
+      in_pos_lim    --> notused, removed
+
+Notes:
+  1) tp->min_pos,max_pos are not used by joint->free_tp, axis->teleop_tp
+     since limits are managed elsewhere in the motion module
+  2) tp->disallow_backoff
+         0: (DEFAULT) allow backoff to mitigate overshoot
+         1: disallow backoff for special cases
+*/
 #include "simple_tp.h"
 #include "rtapi_math.h"
 
-void simple_tp_update(simple_tp_t *tp, double period)
-{
-    double max_dv, tiny_dp, pos_err, vel_req;
+#define SET_NEXT_STATE(out_pos, out_vel, in_pos)          \
+    do {                                                  \
+        tp->curr_pos    = out_pos;                        \
+        tp->out_vel_old = out_vel;                        \
+        tp->curr_vel    = out_vel;                        \
+        tp->in_pos_old  = in_pos;                         \
+        tiny_dp = fabs(TINY_DP(tp->max_acc,fperiod));     \
+        if (fabs(tp->curr_pos - tp->pos_cmd) < tiny_dp) { \
+          tp->active = 0;                                 \
+        }                                                 \
+        return;                                           \
+    } while (0)
 
-    tp->active = 0;
-    /* compute max change in velocity per servo period */
-    max_dv = tp->max_acc * period;
-    /* compute a tiny position range, to be treated as zero */
-    tiny_dp = TINY_DP(tp->max_acc, period);
-    /* calculate desired velocity */
-    if (tp->enable) {
-	/* planner enabled, request a velocity that tends to drive
-	   pos_err to zero, but allows for stopping without position
-	   overshoot */
-	pos_err = tp->pos_cmd - tp->curr_pos;
-	/* positive and negative errors require some sign flipping to
-	   avoid sqrt(negative) */
-	if (pos_err > tiny_dp) {
-	    vel_req = -max_dv +
-		       sqrt(2.0 * tp->max_acc * pos_err + max_dv * max_dv);
-	    /* mark planner as active */
-	    tp->active = 1;
-	} else if (pos_err < -tiny_dp) {
-	    vel_req =  max_dv -
-		       sqrt(-2.0 * tp->max_acc * pos_err + max_dv * max_dv);
-	    /* mark planner as active */
-	    tp->active = 1;
-	} else {
-	    /* within 'tiny_dp' of desired pos, no need to move */
-	    vel_req = 0.0;
-	}
-    } else {
-	/* planner disabled, request zero velocity */
-	vel_req = 0.0;
-	/* and set command to present position to avoid movement when
-	   next enabled */
-	tp->pos_cmd = tp->curr_pos;
+#define VALID_NEXT(pos, vel) ((pos) <= max_pos && (pos) >= min_pos \
+                              && (vel) <= max_vel && (vel) >= min_vel)
+
+void simple_tp_update(simple_tp_t *tp, double fperiod)
+{
+    double in_vel, min_vel, max_vel, min_pos, max_pos;
+    double vel_0_time, vel_0_pos;
+    double vel_match_time, vel_match_in_pos, vel_match_out_pos;
+    int    out_dir, out_dir_rel;
+    double tiny_dp;
+
+    tp->active = 1; // disprove
+
+    if (!tp->enable) {
+        tp->pos_cmd = tp->curr_pos;
+        SET_NEXT_STATE(tp->curr_pos, 0, tp->curr_pos);
     }
-    /* limit velocity request */
-    if (vel_req > tp->max_vel) {
-        vel_req = tp->max_vel;
-    } else if (vel_req < -tp->max_vel) {
-	vel_req = -tp->max_vel;
+
+    // Input velocity
+    in_vel = (tp->pos_cmd - tp->in_pos_old) / fperiod;
+    // Most negative/positive velocity reachable in one period
+    min_vel = fmax(tp->out_vel_old - tp->max_acc * fperiod, -tp->max_vel);
+    max_vel = fmin(tp->out_vel_old + tp->max_acc * fperiod, tp->max_vel);
+    // Most negative/positive position reachable in one period
+    min_pos = tp->curr_pos + min_vel * fperiod;
+    max_pos = tp->curr_pos + max_vel * fperiod;
+
+    // Direction (sign) of output movement
+    out_dir = (tp->out_vel_old < 0) ? -1 : 1;
+    // Direction of output movement relative to input movement
+    out_dir_rel = (tp->out_vel_old - in_vel < 0) ? -1 : 1;
+
+    // Respect max/min position limits:  stop at limit line
+    vel_0_time = fabs(tp->out_vel_old/tp->max_acc); // min time to decel to stop
+    vel_0_pos = tp->curr_pos                        // position after stop
+        + tp->out_vel_old * (vel_0_time+fperiod)
+        + 0.5 * (-out_dir * tp->max_acc) * pow(vel_0_time,2);
+
+    // Follow input signal:  match position and velocity
+    // - min time for velocity match
+    vel_match_time = fabs(tp->out_vel_old-in_vel) / tp->max_acc;
+    // - input position after velocity match
+    vel_match_in_pos = tp->pos_cmd + in_vel * vel_match_time;
+    // - output position after velocity match
+    vel_match_out_pos = tp->curr_pos
+        + tp->out_vel_old * (vel_match_time+fperiod)
+        + 0.5 * (-out_dir_rel * tp->max_acc) * pow(vel_match_time,2);
+
+    // Respect max/min position limits
+    //
+    // - If not at the limit line but in danger of overshooting it,
+    //   slow down
+    if (vel_0_pos >= tp->max_pos && !VALID_NEXT(tp->max_pos,0)) // can't follow max limit
+        SET_NEXT_STATE(min_pos, min_vel, tp->pos_cmd);
+    if (vel_0_pos <= tp->min_pos && !VALID_NEXT(tp->min_pos,0)) // can't follow min limit
+        SET_NEXT_STATE(max_pos, max_vel, tp->pos_cmd);
+    // - If input signal is headed out of bounds, or headed in bounds
+    //   but no danger of overshooting, the limit is the goal
+    if ((vel_match_in_pos < tp->min_pos) // Input below min limit
+        || (tp->pos_cmd <= tp->min_pos && vel_match_in_pos < vel_match_out_pos)) {
+        if (VALID_NEXT(tp->min_pos,0))
+            SET_NEXT_STATE(tp->min_pos, 0, tp->pos_cmd);   // - Park at min limit
+        else
+            SET_NEXT_STATE(min_pos, min_vel, tp->pos_cmd); // - Head toward min limit
     }
-    /* ramp velocity toward request at accel limit */
-    if (vel_req > tp->curr_vel + max_dv) {
-	tp->curr_vel += max_dv;
-    } else if (vel_req < tp->curr_vel - max_dv) {
-	tp->curr_vel -= max_dv;
-    } else {
-	tp->curr_vel = vel_req;
+    if ((vel_match_in_pos > tp->max_pos)                   // Input above max limit
+        || (tp->pos_cmd >= tp->max_pos && vel_match_in_pos > vel_match_out_pos)) {
+        if (VALID_NEXT(tp->max_pos,0))
+            SET_NEXT_STATE(tp->max_pos, 0, tp->pos_cmd);   // - Park at max limit
+        else
+            SET_NEXT_STATE(max_pos, max_vel, tp->pos_cmd); // - Head toward min limit
     }
-    /* check for still moving */
-    if (tp->curr_vel != 0.0) {
-	/* yes, mark planner active */
-	tp->active = 1;
+
+    // Follow input signal
+    //
+    // - Try to track input
+    if (VALID_NEXT(tp->pos_cmd, in_vel))
+        SET_NEXT_STATE(tp->pos_cmd, in_vel, tp->pos_cmd);
+    // - Try to match position and velocity without overshooting
+    if (tp->curr_pos > tp->pos_cmd) {                       // Output > input:
+        if (vel_match_in_pos < vel_match_out_pos)           // - Not overshooting
+            SET_NEXT_STATE(min_pos, min_vel, tp->pos_cmd);  //   - Move closer
+        else                                                // - Overshooting
+            if (tp->disallow_backoff) {
+                SET_NEXT_STATE(min_pos, min_vel, tp->pos_cmd);  // for eoffset_pid
+            } else {
+                SET_NEXT_STATE(max_pos, max_vel, tp->pos_cmd);  //   - Back off
+            }
     }
-    /* integrate velocity to get new position */
-    tp->curr_pos += tp->curr_vel * period;
+    if (tp->curr_pos < tp->pos_cmd) {                       // Output < input
+        if (vel_match_in_pos > vel_match_out_pos)           // - Not overshooting
+            SET_NEXT_STATE(max_pos, max_vel, tp->pos_cmd);  //   - Move closer
+        else                                                // - Overshooting
+            if (tp->disallow_backoff) {
+                SET_NEXT_STATE(max_pos, max_vel, tp->pos_cmd);  // for eoffset_pid
+            } else {
+                SET_NEXT_STATE(min_pos, min_vel, tp->pos_cmd);  //   - Back off
+            }
+    }
 }
